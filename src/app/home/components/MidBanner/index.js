f01bbed7da310b5834c6877165c01a215aa4c2d2
Deepak Kumar <deepakkumar@Deepaks-MacBook-Pro.local>
2025-11-07 07:48:01 +0500

    testing

--- src/app/home/components/MidBanner/index.js
+++ src/app/home/components/MidBanner/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import CounterSection from "./CounterSection";
-import ContentSection from "./ContentSection";
-import VideoSection from "@/components/common/VideoSection";
+// import ContentSection from "./ContentSection";
+// import VideoSection from "@/components/common/VideoSection";
 
 
 
@@ -9,7 +9,7 @@ const MidBanner = () => {
   return (
     <div>
       <CounterSection />
-      <ContentSection />
+      {/* <ContentSection /> */}
       {/* <VideoSection  height={`630px`}/> */}
     </div>
   );
