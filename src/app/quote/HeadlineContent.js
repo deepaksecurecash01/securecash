e7f436d070417d11d032661dbc8555c948c6a6af
Deepak Kumar <deepakkumar@Deepaks-MacBook-Pro.local>
2025-12-02 10:36:10 +0500

    testing

--- src/app/quote/HeadlineContent.js
+++ src/app/quote/HeadlineContent.js
@@ -1,43 +1,45 @@
-import React from 'react'
+import React from 'react';
 
-const HeadlineContent = () => {
-  return (
-        <div
+const HeadlineContent = () =>
+{
+    return (
+        <section
             id="headline-content"
-          className="in-full bg-dots bg-gray w-full h-[406px]  768px:h-[572px] flex justify-center items-center flex-col"
+            className="in-full bg-dots bg-gray w-full h-[406px] 768px:h-[572px] flex justify-center items-center flex-col"
+            aria-labelledby="headline-title"
         >
             <div id="headline-wrapper" className="text-center">
-                <h3
+                <p
                     className="text-[20px] text-[#4d4d4d] leading-[28px] text-center mb-[24px] 
           mx-auto 
-         768px:text-2xl 992px:text-[40px] 992px:leading-[1em]  font-prata
-       "
+          768px:text-2xl 992px:text-[40px] 992px:leading-[1em] font-prata"
                 >
                     Start Taking Advantage Of
-                </h3>
+                </p>
 
                 <h1
+                    id="headline-title"
                     className="mx-auto font-medium text-[40px] leading-[1.2em]
-         text-center 
-         w-[80%] 1024px:w-full 768px:text-5xl 
-         992px:text-[88px] 992px:leading-[1em] mb-6 font-montserrat
-         "
+          text-center 
+          w-[80%] 1024px:w-full 768px:text-5xl 
+          992px:text-[88px] 992px:leading-[1em] mb-6 font-montserrat"
                 >
                     Our Services Today&nbsp;
                 </h1>
                 <hr
-                   
-                  className="1024px:mt-0 mb-6 w-[100px] h-[4px] rounded-[5px] border-0 bg-primary mx-auto"
+                    className="1024px:mt-0 mb-6 w-[100px] h-[4px] rounded-[5px] border-0 bg-primary mx-auto"
+                    aria-hidden="true"
                 />
                 <p
                     className="text-[16px] text-[#4d4d4d] font-normal leading-[24px] text-center mb-4 
-         w-[86%] 768px:text-xl mx-auto 
-         1024px:w-full 1024px:mx-0 992px:text-[32px] font-montserrat"
+          w-[86%] 768px:text-xl mx-auto 
+          1024px:w-full 1024px:mx-0 992px:text-[32px] font-montserrat"
                 >
                     Get a quote email within 45 minutes.{" "}
                 </p>
             </div>
-        </div>);
-}
+        </section>
+    );
+};
 
-export default HeadlineContent
+export default HeadlineContent;
--- src/app/quote/QuoteContent.js
+++ src/app/quote/QuoteContent.js
@@ -4,61 +4,59 @@ import { FaPhoneAlt, FaMapMarkerAlt, FaEnvelope, FaStar } from "react-icons/fa";
 import Link from "next/link";
 import QuoteForm from "@/components/common/forms-new/forms/QuoteForm";
 
-const QuoteContent = () => {
+const QuoteContent = () =>
+{
   return (
-    <div id="quote-content">
-      <Container id="quote-content-container" className="w-[95%]  1440px:w-full 992px:flex">
+    <section id="quote-content" aria-labelledby="quote-heading">
+      <Container id="quote-content-container" className="w-[95%] 1440px:w-full 992px:flex">
         <div id="quote-content-left" className="[flex-1] overflow-hidden">
           <div
             id="left-content-text-wrapper"
-            className="px-3 py-[40px] flex flex-col mt-[250px]  1024px:mt-[360px] pb-[20px] pt-[54px]"
+            className="px-3 py-[40px] flex flex-col mt-[250px] 1024px:mt-[360px] pb-[20px] pt-[54px]"
           >
             <div id="left-content-header-wrapper" className="mb-10">
-              <h3
-                className=" mx-auto font-medium text-center text-[24px]  768px:text-[32px] 1100px:text-[36px]  1200px:text-[40px] mb-[8px] leading-[1.4em] -mt-[10px]  font-montserrat"
+              <h2
+                id="quote-heading"
+                className="mx-auto font-medium text-center text-[24px] 768px:text-[32px] 1100px:text-[36px] 1200px:text-[40px] mb-[8px] leading-[1.4em] -mt-[10px] font-montserrat"
               >
                 Get in touch with our <br />
                 Business Development Team
-              </h3>
+              </h2>
               <hr
                 className="1024px:mt-0 mt-4 1024px:mb-6 w-[100px] h-[4px] rounded-[5px] border-0 bg-primary mx-auto"
+                aria-hidden="true"
               />
             </div>
-            <div id="contact-info-wrapper" className="480px:flex  992px:pb-10">
-              <div className="contact-info-item   768px:max-w-[50%]">
-                <h4
+            <div id="contact-info-wrapper" className="480px:flex 992px:pb-10">
+              <div className="contact-info-item 768px:max-w-[50%]">
+                <h3
                   className="mx-auto font-medium text-[24px] leading-[1.4em]
-          flex items-center justify-center 992px:justify-start font-montserrat
-         "
+                  flex items-center justify-center 992px:justify-start font-montserrat"
                 >
-                  <FaMapMarkerAlt className="pr-2.5 text-[26px] relative inline text-primary" />
+                  <FaMapMarkerAlt className="pr-2.5 text-[26px] relative inline text-primary" aria-hidden="true" />
                   Address
-                </h4>
+                </h3>
                 <p
-                  
-                  className="text-center 992px:text-left font-light leading-[2em] mb-2  mt-2 font-montserrat"
+                  className="text-center 992px:text-left font-light leading-[2em] mb-2 mt-2 font-montserrat"
                 >
                   Anywhere, Anytime, Australia Wide! No matter where you are
                   located in Australia we will be able to organise someone to
                   service your location.
                 </p>
               </div>
-              <div className="mid-row-divider-wrapper bg-transparent [flex-1] items-end flex ml-[16px] mr-[16px]">
+              <div className="mid-row-divider-wrapper bg-transparent [flex-1] items-end flex ml-[16px] mr-[16px]" aria-hidden="true">
                 <div className="mid-row-divider self-end bg-[#dddddd] w-px h-full"></div>
               </div>
-              <div className="contact-info-item  mt-8 480px:mt-0   768px:max-w-[50%]">
-                <h4
+              <div className="contact-info-item mt-8 480px:mt-0 768px:max-w-[50%]">
+                <h3
                   className="mx-auto font-medium text-[24px] leading-[1.4em]
-          flex items-center justify-center 992px:justify-start font-montserrat
-       
-         "
+                  flex items-center justify-center 992px:justify-start font-montserrat"
                 >
-                  <FaEnvelope className="pr-2.5 text-[26px] relative inline text-primary" />
+                  <FaEnvelope className="pr-2.5 text-[26px] relative inline text-primary" aria-hidden="true" />
                   Email Us
-                </h4>
+                </h3>
                 <p
-                
-                  className="text-center 992px:text-left font-light leading-[2em] mb-2  mt-2 font-montserrat"
+                  className="text-center 992px:text-left font-light leading-[2em] mb-2 mt-2 font-montserrat"
                 >
                   You can reach our Business Development&nbsp; Team directly by
                   emailing;
@@ -76,23 +74,22 @@ const QuoteContent = () => {
             </div>
             <div className="qoute-callback-cta-desktop hidden 992px:block">
               <div id="note-wrapper" className="mb-4">
-                <h4
-                  className="mx-auto font-medium text-[20px] 1070px:text-[22px]  1200px:text-[24px] leading-[1.4em] text-center font-montserrat
-         "
+                <p
+                  className="mx-auto font-medium text-[20px] 1070px:text-[22px] 1200px:text-[24px] leading-[1.4em] text-center font-montserrat"
                 >
                   Would You Rather Talk To Us Over The Phone?
-                </h4>
+                </p>
                 <p
-              
                   className="text-center font-light leading-[2em] mb-2 font-montserrat"
                 >
                   Request a call back at a time which is convenient for you!
                 </p>
               </div>
-              <div className=" flex justify-center items-center">
+              <div className="flex justify-center items-center">
                 <Link
                   href="/contact/#contact-form-section"
                   className="nextBtn w-[200px] bg-[#c6a54b] text-white border-none text-center cursor-pointer rounded-[40px] outline-none appearance-none hover:opacity-80-sm px-2 py-4 shadow-none font-montserrat hover:bg-black"
+                  aria-label="Request a call back from our team"
                 >
                   Request a Call Back
                 </Link>
@@ -105,30 +102,29 @@ const QuoteContent = () => {
         </div>
         <div className="pt-10 w-[90%] mx-auto 480px:w-full 992px:hidden">
           <div id="note-wrapper" className="mb-4">
-            <h4
-              className="mx-auto font-medium text-[20px] 1070px:text-[22px]  1200px:text-[24px] leading-[1.4em] text-center font-montserrat
-         "
+            <p
+              className="mx-auto font-medium text-[20px] 1070px:text-[22px] 1200px:text-[24px] leading-[1.4em] text-center font-montserrat"
             >
               Would You Rather Talk To Us Over The Phone?
-            </h4>
+            </p>
             <p
-            
               className="text-center font-light leading-[2em] mb-2 font-montserrat"
             >
               Request a call back at a time which is convenient for you!
             </p>
           </div>
-          <div className=" flex justify-center items-center">
+          <div className="flex justify-center items-center">
             <Link
               href="/contact/#contact-form-section"
               className="nextBtn w-[200px] bg-[#c6a54b] text-white border-none text-center cursor-pointer rounded-[40px] outline-none appearance-none hover:opacity-80-sm px-2 py-4 shadow-none font-montserrat hover:bg-black"
+              aria-label="Request a call back from our team"
             >
               Request a Call Back
             </Link>
           </div>
         </div>
       </Container>
-    </div>
+    </section>
   );
 };
 
